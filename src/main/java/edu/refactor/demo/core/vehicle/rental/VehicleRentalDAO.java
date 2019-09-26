dac950bd0e32997c21395155512099a6ea1cf29b
Olga Zdobnova <zdobnova@haulmont.com>
2019-09-26 10:46:49 +0400

    Fixed a VehicleRentalDAO

--- src/main/java/edu/refactor/demo/core/vehicle/rental/VehicleRentalDAO.java
+++ src/main/java/edu/refactor/demo/core/vehicle/rental/VehicleRentalDAO.java
@@ -12,19 +12,22 @@ import java.util.Optional;
 @Repository
 public interface VehicleRentalDAO extends CrudRepository<VehicleRental, Long> {
 
-    String UPDATE_CUSTOMER_STATUS = "update customer c set c.status = 'expired' where c.id=(select cust.id from vehicle_rental r join customer cust on cust.id=r.customer_id " +
-        "where r.status='active' and ((datediff(msc, now(), r.start_rent) > 86400 and c.status='default') or (datediff(msc, now(), r.start_rent) > 259200 and c.status='vip'))";
-    String FIND_ACTIVE_BY_ID = "select r from vehicleRental r where id=:id and status='active'";
-    String UPDATE_STATUS = "UPDATE vehicleRental r SET r.status = 'active', r.endRent=now() WHERE r.id=:id and (r.status='created' or r.status ='expired')";
+    String ID = "id";
+    String UPDATE_CUSTOMER_STATUS = "update customer c set c.status = 'expired' where c.id=" +
+        "(select cust.id from vehicle_rental r join customer cust on cust.id=r.customer_id " +
+        "where r.status='active' and ((datediff(msc, now(), r.start_rent) > 86400 and c.status='default') " +
+        "or (datediff(msc, now(), r.start_rent) > 259200 and c.status='vip'))";
+    String FIND_ACTIVE_BY_ID = "select r from VehicleRental r where r.id=:" + ID + " and r.status='active'";
+    String UPDATE_STATUS = "update VehicleRental r set r.status = 'active', r.endRent=now() where r.id=:" + ID + " and (r.status='created' or r.status ='expired')";
 
     @Query(FIND_ACTIVE_BY_ID)
-    Optional<VehicleRental> findActiveById(@Param("id") Long id);
+    Optional<VehicleRental> findActiveById(@Param(ID) Long id);
 
-    @Query(UPDATE_CUSTOMER_STATUS)
+    @Query(value = UPDATE_CUSTOMER_STATUS, nativeQuery = true)
     @Modifying(clearAutomatically = true)
     void updateCustomerStatus();
 
-    @Query(value = UPDATE_STATUS, nativeQuery = true)
+    @Query(value = UPDATE_STATUS)
     @Modifying(clearAutomatically = true)
-    void activate(@Param("id") Long id);
+    void activate(@Param(ID) Long id);
 }
